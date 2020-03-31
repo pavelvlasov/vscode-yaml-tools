522f3d0d4fa05d63de016307c1b41e27d6810be1
Pavel Vlasov <freakycue@gmail.com>
2020-03-31 16:37:52 +1100

    ignore schema errors

--- src/services/validation/index.ts
+++ src/services/validation/index.ts
@@ -1,100 +1,74 @@
+import { JSONSchema } from "vscode-json-languageservice"
 import {
-	ErrorCode,
-	Problem,
-	YAMLDocument
-} from "../../parser"
-import {
-	Diagnostic,
-	DiagnosticSeverity,
-	TextDocument
+    Diagnostic,
+    DiagnosticSeverity,
+    TextDocument,
 } from "vscode-languageserver"
 
+import { ErrorCode, Problem, YAMLDocument } from "../../parser"
 import { removeDuplicatesObj } from "../../utils/array"
-import { JSONSchema } from "vscode-json-languageservice"
 
 export class YAMLValidation {
     private schema: JSONSchema
 
-	constructor(
-        schema: JSONSchema,
-	) {
+    constructor(schema: JSONSchema) {
         this.schema = schema
-	}
+    }
 
-	async doValidation(textDocument: TextDocument, yamlDocument: YAMLDocument) {
-		return await this.validateWithSchema(textDocument, yamlDocument)
-	}
+    async doValidation(textDocument: TextDocument, yamlDocument: YAMLDocument) {
+        return await this.validateWithSchema(textDocument, yamlDocument)
+    }
 
-	private async validateWithSchema(
-		textDocument: TextDocument,
-		yamlDocument: YAMLDocument,
-	): Promise<Diagnostic[]> {
-		let diagnostics: Diagnostic[] = []
-		const added: {[key: string]: boolean} = {}
+    // eslint-disable-next-line @typescript-eslint/require-await
+    private async validateWithSchema(
+        textDocument: TextDocument,
+        yamlDocument: YAMLDocument
+    ): Promise<Diagnostic[]> {
+        const diagnostics: Diagnostic[] = []
+        const added: { [key: string]: boolean } = {}
 
-		const currentDocProblems = yamlDocument.getValidationProblems(
+        const currentDocProblems = yamlDocument.getValidationProblems(
             this.schema
         )
-        currentDocProblems.forEach(problem => {
+        currentDocProblems.forEach((problem) => {
             yamlDocument.errors.push({
                 location: {
                     start: problem.location.start,
-                    end: problem.location.end
+                    end: problem.location.end,
                 },
                 message: problem.message,
-                code: ErrorCode.Undefined
+                code: ErrorCode.Undefined,
             })
         })
 
-        // @ts-ignore
-        if (this.schema && this.schema.errors.length > 0) {
-            // @ts-ignore
-            this.schema.errors.forEach(error => {
+        const addProblem = (
+            { message, location }: Problem,
+            severity: DiagnosticSeverity
+        ) => {
+            const signature = `${location.start} ${location.end} ${message}`
+            // remove duplicated messages
+            if (!added[signature]) {
+                added[signature] = true
+                const range = {
+                    start: textDocument.positionAt(location.start),
+                    end: textDocument.positionAt(location.end),
+                }
                 diagnostics.push({
-                    severity: DiagnosticSeverity.Error,
-                    range: {
-                        start: {
-                            line: 0,
-                            character: 0
-                        },
-                        end: {
-                            line: 0,
-                            character: 1
-                        }
-                    },
-                    message: error
+                    severity,
+                    range,
+                    message: `[Serverless IDE] ${message}`,
                 })
-            })
+            }
         }
 
-		const addProblem = (
-			{ message, location }: Problem,
-			severity: DiagnosticSeverity
-		) => {
-			const signature = `${location.start} ${location.end} ${message}`
-			// remove duplicated messages
-			if (!added[signature]) {
-				added[signature] = true
-				const range = {
-					start: textDocument.positionAt(location.start),
-					end: textDocument.positionAt(location.end)
-				}
-				diagnostics.push({
-					severity,
-					range,
-					message: `[Serverless IDE] ${message}`
-				})
-			}
-		}
+        yamlDocument.errors.forEach((error) => {
+            addProblem(error, DiagnosticSeverity.Error)
+        })
 
-		yamlDocument.errors.forEach(error => {
-			addProblem(error, DiagnosticSeverity.Error)
-		})
+        yamlDocument.warnings.forEach((warning) => {
+            addProblem(warning, DiagnosticSeverity.Warning)
+        })
 
-		yamlDocument.warnings.forEach(warning => {
-			addProblem(warning, DiagnosticSeverity.Warning)
-		})
-
-		return removeDuplicatesObj(diagnostics)
-	}
+        return removeDuplicatesObj(diagnostics)
+    }
 }
